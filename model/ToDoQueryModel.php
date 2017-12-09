ae602ffd80ed47435399c16368c78decc4f17dc6
monicasangam <32145733+monicasangam@users.noreply.github.com>
2017-12-09 18:47:04 -0500

    added todo query model using namespace model

---
+++ model/ToDoQueryModel.php
@@ -0,0 +1,26 @@
+<?php
+namespace model;
+
+ini_set('display_errors', on);
+error_reporting(E_ALL);
+define('DATABASE','ms792' );
+define('USERNAME', 'ms792');
+define('PASSWORD', 'bSzrOJUh');
+define('CONNECTION', 'sql1.njit.edu');
+
+include "model/GenericQueryModel.php";
+
+class ToDoQueryModel extends GenericQueryModel
+{
+    public $id;
+    public $ownerEmail;
+    public $ownerId;
+    public $createdDate;
+    public $dueDate;
+    public $message;
+    public function __construct()
+    {
+        $this->tableName = 'todos';
+    }
+}
+?>

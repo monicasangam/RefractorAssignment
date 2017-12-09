49d13513079c9998d749e036ed273b37221ec1e0
monicasangam <32145733+monicasangam@users.noreply.github.com>
2017-12-09 18:45:18 -0500

    added generic query model using namespace model

---
+++ model/GenericQueryModel.php
@@ -0,0 +1,17 @@
+<?php
+namespace model;
+ini_set('display_errors', on);
+error_reporting(E_ALL);
+define('DATABASE','ms792' );
+define('USERNAME', 'ms792');
+define('PASSWORD', 'bSzrOJUh');
+define('CONNECTION', 'sql1.njit.edu');
+
+
+
+class GenericQueryModel
+{
+    public $tableName;
+}
+
+?>
